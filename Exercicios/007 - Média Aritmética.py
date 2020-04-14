37ec3858c4f530c9c309b8b5836015bae50184d5
Isac Heres Lopes <isachlopes2@hotmail.com>
2020-04-13 21:33:07 -0300

    fiz mais algumas mudanças

--- Exercicios/007 - Média Aritmética.py
+++ Exercicios/007 - Média Aritmética.py
@@ -1,5 +1,4 @@
 #
 n = float(input('Digite a primeira nota do aluno: '))
 m = float(input('Digite a segunda nota do aluno: '))
-x = (n+m)/2
-print(' A média de {} e {} é igual a {}.'.format(n, m, x))
+print(f' A média de {n} e {m} é igual a {(n+m)/2}.')
