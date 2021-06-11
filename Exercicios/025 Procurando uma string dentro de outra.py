bc5c9dbaec50b6b97566d9b2c131a11e5e2ba2da
isachlopes <isachlopes2@hotmail.com>
2021-06-10 21:18:14 -0300

    02

---
+++ Exercicios/025 Procurando uma string dentro de outra.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nome = str(input('Digite o seu nome completo: ')).strip().lower()
+silva = nome.strip()
+
+print(silva)
+
