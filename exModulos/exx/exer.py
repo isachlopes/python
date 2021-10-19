b3814c408b13ff404bc73c8cac1367f66cd83792
Isac Heres Lopes <isachlopes2@hotmail.com>
2021-10-19 13:54:28 -0300

    Up_67

--- exModulos/exx/exer.py
+++ exModulos/exx/exer.py
@@ -9,13 +9,17 @@ if not arquivoexiste(arq):
 
 while True:
     resp = menu(['Ver Pessoas Cadastradas', 'Cadastrar Novas Pessoas', 'Sair do Sistema'])
-    if resp == 1:
-        cabeçalho('Opção 1')
-    elif resp == 2:
-        cabeçalho('Opção 2')
+    if resp == 1:#opção de listar o conteudo de um arquivo
+        lerarquivo(arq)
+    elif resp == 2:#CADASTRAR UMA NOVA PESSOA
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resp == 3:
         cabeçalho('Saindo do sistema. Até logo!...')
         break
     else:
         print('\033[31mErro! Digite uma opção válida!\033[m')
     sleep(1)
+    
--- exModulos/exx/modulos/arquivo/__init__.py
+++ exModulos/exx/modulos/arquivo/__init__.py
@@ -1,3 +1,6 @@
+from modulos.lib import *
+
+
 def arquivoexiste(nome):
     try:
         a = open(nome, 'rt')
@@ -16,3 +19,31 @@ def criararquivo(nome):
     else:
         print(f'Arquivo {nome} criado com sucesso!')
         
+def lerarquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<34}{dado[1]:>3} anos')
+    finally:
+        a.close
+        
+        
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo.')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('houve um erro na hora de escrever os dado!')
+        else:
+            print(f'Novo registro de {nome} adicionado!')
+            
