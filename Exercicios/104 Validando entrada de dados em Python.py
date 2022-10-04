e84d6b8f65b63965789edfc64cfa3cbe52a468c3
Isac Heres Lopes <44485777+isachlopes@users.noreply.github.com>
2022-10-04 12:34:21 +0000

    comit

--- Exercicios/104 Validando entrada de dados em Python.py
+++ Exercicios/104 Validando entrada de dados em Python.py
@@ -1,4 +1,6 @@
 #Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt('Digite um n: ')
+
+
 def leiaint(msg):
     global n
     while True:
